0ab0325d29d75fe51f9c525b0451072f39d4c0de
tryoma <tryoma0326@gmail.com>
2020-03-17 13:47:02 +0000

    日時の設定を追加

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ Bundler.require(*Rails.groups)
 module AttendanceApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
+    config.load_defaults 5.
+    config.time_zone = 'Asia/Tokyo' # この１行をここに追加します。
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
