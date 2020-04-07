97c1c7bb827f723307799ba76482ae2550be849f
tryoma <tryoma0326@gmail.com>
2020-04-07 13:58:52 +0000

    base

--- app/controllers/bases_controller.rb
+++ app/controllers/bases_controller.rb
@@ -1,2 +1,23 @@
 class BasesController < ApplicationController
+  def index
+    @bases = Base.all
+  end
+  
+  def new
+    @base = Base.new
+  end
+  
+  def create
+    @base = Base.new(base_params)
+    if @base.save
+      flash[:success] = '拠点登録しました。'
+      redirect_to 
+    else
+      render :new
+    end
+  end
+  
+  def base_params
+    params.require(:base).permit(:base_number, :base_name, :base_type)
+  end
 end
--- app/models/base.rb
+++ app/models/base.rb
@@ -1,2 +1,4 @@
 class Base < ApplicationRecord
+  validates :base_number,  presence: true, length: { maximum: 50 } 
+  validates :base_name,  presence: true, length: { maximum: 50 } 
 end
