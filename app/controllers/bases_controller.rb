c0abdb5b24af234c157cdc86a7c8bfba20be9838
tryoma <tryoma0326@gmail.com>
2020-04-08 13:46:10 +0000

    base-editing-ok

--- app/controllers/bases_controller.rb
+++ app/controllers/bases_controller.rb
@@ -1,6 +1,20 @@
 class BasesController < ApplicationController
+  before_action :set_base, only: [:edit, :update, :destroy]
+  
   def index
     @bases = Base.all
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @base.update_attributes(base_params)
+      flash[:success] = "拠点の更新に成功しました。"
+      redirect_to bases_url
+    else
+      render :edit
+    end
   end
   
   def new
@@ -11,13 +25,26 @@ class BasesController < ApplicationController
     @base = Base.new(base_params)
     if @base.save
       flash[:success] = '拠点登録しました。'
-      redirect_to 
-    else
+      redirect_to bases_url
+    else 
       render :new
     end
   end
   
+  def destroy
+    @base.destroy
+    flash[:success] = "#{@base.base_name}のデータを削除しました。"
+    redirect_to bases_url
+  end
+  
+  private
+  
   def base_params
     params.require(:base).permit(:base_number, :base_name, :base_type)
   end
+  
+  def set_base
+    @base = Base.find(params[:id])
+  end
+    
 end
--- app/models/base.rb
+++ app/models/base.rb
@@ -1,4 +1,4 @@
 class Base < ApplicationRecord
-  validates :base_number,  presence: true, length: { maximum: 50 } 
-  validates :base_name,  presence: true, length: { maximum: 50 } 
+  validates :base_number,  presence: true
+  validates :base_name,  presence: true
 end
