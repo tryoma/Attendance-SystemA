7151d71ef570807e78463b3eb6fb3711aa20c997
tryoma <tryoma0326@gmail.com>
2020-04-10 12:53:00 +0000

    user-editing

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :bases
+  resources :bases do
+  end
 
   resources :users do
     member do
